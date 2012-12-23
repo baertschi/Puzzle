5ffb65c37a1872b468f8bdb1ce3d97579f175551
Cyril Stoller <cyril.stoller@gmail.com>
2012-12-23 20:50:47 +0100

    added corner detection from the approximated contours TODO: line 113 istn't working - there's a problem with the uninitialized variable 'corners'

--- Puzzle/main.cpp
+++ Puzzle/main.cpp
@@ -1,13 +1,24 @@
 #include <opencv2/opencv.hpp>
 #include <stdlib.h>
+#include <iostream>
+#include <fstream>
 
-void approx(int, void* );
+// Helper Funktion:
+// Cosinus des Zwischenwinkels von Vektor pt0->pt1 und Vektor pt0->pt2
+double angle(cv::Point pt1, cv::Point pt2, cv::Point pt0)
+{
+    double dx1 = pt1.x - pt0.x;
+    double dy1 = pt1.y - pt0.y;
+    double dx2 = pt2.x - pt0.x;
+    double dy2 = pt2.y - pt0.y;
+    return (dx1*dx2 + dy1*dy2)/sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
+}
 
 int main(int argc, char *argv[])
 {
     // wenn kein argument mit dem Dateinamen mitgegeben wird:
     // Dateiname = "puzzle.jpg"
-    char* filename = "puzzle.jpg";
+    const char* filename = "puzzle.jpg";
     if(argc == 2)
     {
         filename = argv[1];
@@ -54,17 +65,7 @@ int main(int argc, char *argv[])
         }
     }
 
-    // Minimal umschliessendes Rechteck bestimmen
-    std::vector<cv::RotatedRect> rectMin(contours.size());
-
-    for(unsigned int i = 0; i < contours.size(); i++)
-    {
-        rectMin[i] = cv::minAreaRect(cv::Mat(contours[i]));
-    }
-
-
-
-    // Zeichnen jeder Kontur und jedes Rechtecks
+    // Zeichnen jeder Kontur
     cv::Mat imgCont = img.clone();
     //cv::RNG rng(12345);
     for(unsigned int i = 0; i< contours.size(); i++)
@@ -73,26 +74,50 @@ int main(int argc, char *argv[])
         //cv::Scalar color = cv::Scalar(rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255));   // zufallsfarben
         cv::Scalar color = cv::Scalar(255,50,0);
         cv::drawContours(imgCont, contours, i, color, 10, 8);
-        // Rechteck zeichnen
-        cv::Point2f pointsRect[4];
-        rectMin[i].points(pointsRect);
-        for(int j = 0; j < 4; j++)
+    }
+
+    // Konturen filtern
+    std::vector<std::vector<cv::Point> > pointsApprox;
+    std::vector<cv::Point> pointApproxTemp;
+    for(unsigned int i = 0; i < contours.size(); i++)
+    {
+        cv::approxPolyDP(cv::Mat(contours[i]), pointApproxTemp, 10, true);
+        pointsApprox.push_back(pointApproxTemp);
+
+        // gefilterte Kontur zeichnen
+        const cv::Point* p = &pointsApprox[i][0];
+        int n = (int)pointsApprox[i].size();
+        cv::polylines(imgCont, &p, &n, 1, true, cv::Scalar(0,255,255), 2, CV_AA);
+    }
+
+    std::vector<std::vector<cv::Point> > corners;
+
+    unsigned int number = 3;
+    std::fstream datei("contour3_angle.txt", std::ios::out);
+
+    // Ecken erkennen
+    for(unsigned int i = 0; i < contours.size(); i++)
+    {
+        for(unsigned int j = 0; j < pointsApprox[i].size(); j++)
         {
-            cv::line(imgCont, pointsRect[j], pointsRect[(j+1)%4], cv::Scalar(0,0,120), 4, CV_AA);
+            // Winkel berechnen zwischen diesem und den zwei nächsten Vertices.
+            double cosine = std::fabs(angle(pointsApprox[i][(j+2)%pointsApprox[i].size()], pointsApprox[i][j], pointsApprox[i][(j+1)%pointsApprox[i].size()]));
+
+            if(i == number)
+            {
+                datei << pointsApprox[number][j].x << '\t' << pointsApprox[number][j].y << '\t' << cosine << std::endl;
+            }
+
+            if(cosine < 0.15)
+            {
+                //corners[i].push_back(pointsApprox[i][(j+1)%pointsApprox[i].size()]);
+                std::cout << "figure " << i << ": " << pointsApprox[i][(j+1)%pointsApprox[i].size()].x << ' ' << pointsApprox[i][(j+1)%pointsApprox[i].size()].y << std::endl;
+                cv::ellipse(imgCont, pointsApprox[i][(j+1)%pointsApprox[i].size()], cv::Size(10,10), 0, 0, 360, cv::Scalar(0,0,255), 2, CV_AA);
+            }
         }
     }
 
-    // Kontur filtern
-    std::vector<cv::Point> pointsApprox;
-    for( size_t i = 0; i < contours.size(); i++ )
-    {
-        cv::approxPolyDP(cv::Mat(contours[i]), pointsApprox, 10, true);
-
-        // gefilterte Kontur zeichnen
-        const cv::Point* p = &pointsApprox[0];
-        int n = (int)pointsApprox.size();
-        cv::polylines(imgCont, &p, &n, 1, true, cv::Scalar(0,255,255), 2, CV_AA);
-    }
+    datei.close();
 
     // Fenster erstellen
     cv::namedWindow("Kontur", 0);
