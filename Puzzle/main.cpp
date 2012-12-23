565d526a077462c24fda38727e758aea0c699b22
Cyril Stoller <cyril.stoller@gmail.com>
2012-12-23 16:27:28 +0100

    added contour filtering with approxPolyDP

--- Puzzle/main.cpp
+++ Puzzle/main.cpp
@@ -1,45 +1,52 @@
 #include <opencv2/opencv.hpp>
+#include <stdlib.h>
+
+void approx(int, void* );
 
 int main(int argc, char *argv[])
 {
-    // Bild laden
-    cv::Mat img = cv::imread("puzzle.jpg");
+    // wenn kein argument mit dem Dateinamen mitgegeben wird:
+    // Dateiname = "puzzle.jpg"
+    char* filename = "puzzle.jpg";
+    if(argc == 2)
+    {
+        filename = argv[1];
+    }
 
-    // Fenster erstellen
-    cv::namedWindow("Binary", 0);
-    cv::namedWindow("Kontur", 0);
+    // Bild laden
+    cv::Mat img = cv::imread(filename);
 
     // Grauwert Bild erzeugen
     cv::Mat imgGrey;
     cv::cvtColor(img, imgGrey, CV_BGR2GRAY);
 
+    // Weichzeichnen (vor-entrauschen)
+    cv::Mat imgGreyBlur;
+    cv::blur(imgGrey, imgGreyBlur, cv::Size(3,3));
 
     // Binarisieren
-    cv::Mat imgThresh;
-    cv::threshold(imgGrey, imgThresh, 30, 255, cv::THRESH_BINARY);
+    cv::Mat imgBin;
+    cv::threshold(imgGreyBlur, imgBin, 30, 255, cv::THRESH_BINARY);
 
-    // Entrauschen: 2x "öffnen" mit 3x3 Kernel
+    // Entrauschen: 2x öffnen mit 3x3 Kernel
     cv::Mat kernel = cv::getStructuringElement(cv::MORPH_CROSS, cv::Size(3, 3), cv::Point(1, 1));
-    cv::morphologyEx(imgThresh, imgThresh, cv::MORPH_OPEN, kernel, cv::Point(-1,-1), 2);
-
-    // Bild anzeigen
-    cv::imshow("Binary", imgThresh);
+    cv::morphologyEx(imgBin, imgBin, cv::MORPH_OPEN, kernel, cv::Point(-1,-1), 2);
 
     // Kantenerkennung:
     //cv::Mat imgEdge;
-    //cv::Canny(imgThresh, imgEdge, 50, 160);
+    //cv::Canny(imgBin, imgEdge, 50, 50);
 
     // Konturen suchen
-    cv::Mat imgThreshTemp = imgThresh;
+    cv::Mat imgBinTemp = imgBin;
     std::vector<std::vector<cv::Point> > contours;
-    cv::findContours(imgThreshTemp, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+    cv::findContours(imgBinTemp, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
 
-    // Gesamte Liste mit den Konturen durchgehen
+    // Gesamte Liste mit den Konturen durchgehen: zu kurze Konturen löschen
     for(unsigned int i = 0; i < contours.size(); ++i)
     {
         double featArcLength = cv::arcLength(cv::Mat(contours[i]), true);
 
-        // Kontur aus der Liste löschen sofern die Länge zu klein
+        // Kontur aus der Liste löschen sofern sie zu kurz ist
         if(featArcLength < 50)
         {
             contours.erase(contours.begin()+i);  // Kontur löschen
@@ -48,29 +55,47 @@ int main(int argc, char *argv[])
     }
 
     // Minimal umschliessendes Rechteck bestimmen
-    std::vector<cv::RotatedRect> minRect(contours.size());
+    std::vector<cv::RotatedRect> rectMin(contours.size());
 
     for(unsigned int i = 0; i < contours.size(); i++)
     {
-        minRect[i] = cv::minAreaRect(cv::Mat(contours[i]));
+        rectMin[i] = cv::minAreaRect(cv::Mat(contours[i]));
     }
 
 
 
     // Zeichnen jeder Kontur und jedes Rechtecks
-    cv::Mat imgCont = img;
+    cv::Mat imgCont = img.clone();
+    //cv::RNG rng(12345);
     for(unsigned int i = 0; i< contours.size(); i++)
-       {
-            // Kontur
-            cv::drawContours(imgCont, contours, i, cv::Scalar(255,0,0), 2, 8);
-            // Rechteck
-            cv::Point2f rect_points[4];
-            minRect[i].points(rect_points);
-            for(int j = 0; j < 4; j++)
-            {
-                cv::line(imgCont, rect_points[j], rect_points[(j+1)%4], cv::Scalar(0,0,255), 2, CV_AA);
-            }
-       }
+    {
+        // Kontur zeichnen
+        //cv::Scalar color = cv::Scalar(rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255));   // zufallsfarben
+        cv::Scalar color = cv::Scalar(255,50,0);
+        cv::drawContours(imgCont, contours, i, color, 10, 8);
+        // Rechteck zeichnen
+        cv::Point2f pointsRect[4];
+        rectMin[i].points(pointsRect);
+        for(int j = 0; j < 4; j++)
+        {
+            cv::line(imgCont, pointsRect[j], pointsRect[(j+1)%4], cv::Scalar(0,0,120), 4, CV_AA);
+        }
+    }
+
+    // Kontur filtern
+    std::vector<cv::Point> pointsApprox;
+    for( size_t i = 0; i < contours.size(); i++ )
+    {
+        cv::approxPolyDP(cv::Mat(contours[i]), pointsApprox, 10, true);
+
+        // gefilterte Kontur zeichnen
+        const cv::Point* p = &pointsApprox[0];
+        int n = (int)pointsApprox.size();
+        cv::polylines(imgCont, &p, &n, 1, true, cv::Scalar(0,255,255), 2, CV_AA);
+    }
+
+    // Fenster erstellen
+    cv::namedWindow("Kontur", 0);
 
     // Bild anzeigen
     cv::imshow("Kontur", imgCont);
@@ -78,5 +103,3 @@ int main(int argc, char *argv[])
     // Warten auf einen Tastendruck
     cv::waitKey(0);
 }
-
-
