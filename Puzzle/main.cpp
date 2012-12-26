a9560dc224764ed740619a003167750fc1c3ae01
Cyril Stoller <cyril.stoller@gmail.com>
2012-12-26 10:44:20 +0100

    approx-test branch: added another image, took better binarisation from master branch

--- Puzzle/main.cpp
+++ Puzzle/main.cpp
@@ -4,8 +4,8 @@
 #include <stdlib.h>
 
 cv::RNG rng(12345);
-int thresh = 5;
-int max_thresh = 200;
+int thresh = 14;
+int max_thresh = 40;
 cv::Mat imgCont;
 std::vector<std::vector<cv::Point> > contours;
 
@@ -13,35 +13,72 @@ void approx(int, void* );
 
 int main(int argc, char *argv[])
 {
-    // Bild laden
-    cv::Mat img = cv::imread("puzzle.jpg");
+    // wenn kein argument mit dem Dateinamen mitgegeben wird:
+    // Dateiname = "puzzle.jpg"
+    const char* filename = "puzzle.jpg";
+    if(argc == 2)
+    {
+        filename = argv[1];
+    }
 
-    // Fenster erstellen
-    cv::namedWindow("Binary", 0);
-    cv::namedWindow("Kontur", 1);
+    // Bild laden
+    cv::Mat img = cv::imread(filename);
+
+    // Wenn kein Bild gelesen wurde, programm beenden
+    if(img.data == NULL)
+    {
+        cv::namedWindow("Error", 0);
+        img = cv::imread("Error.png");                  // Bild im Bin ordner hinzufügen
+        cv::imshow("Error", img);
+        std::cout << "Error: Can't read image" << std::endl;
+        cv::waitKey(0);
+
+        // wär gloub no suber weme d fänster würd destroye, het aber d funktion nid kennt
+        // cv::destroyAllWindows();
+        return 0;
+    }
+
+    // Originalbild anzeigen
+    cv::namedWindow("original", 0);
+    cv::imshow("original", img);
 
     // Grauwert Bild erzeugen
     cv::Mat imgGrey;
     cv::cvtColor(img, imgGrey, CV_BGR2GRAY);
+    //cv::namedWindow("grey", 0);                 // Für debugging
+    //cv::imshow("grey", imgGrey);                // Für debugging
+
+    // Histogramm ausgleichen
+    //cv::Mat imgGreyHist;
+    //cv::equalizeHist(imgGrey,imgGreyHist);
+    //cv::namedWindow("hist", 0);
+    //cv::imshow("hist", imgGreyHist);
+
+    // Weichzeichnen (vor-entrauschen)
+    cv::Mat imgGreyBlur;
+    cv::blur(imgGrey, imgGreyBlur, cv::Size(3,3));
+    //cv::namedWindow("blur", 0);                 // Für debugging
+    //cv::imshow("blur", imgGreyBlur);            // Für debugging
 
 
     // Binarisieren
-    cv::Mat imgThresh;
-    cv::threshold(imgGrey, imgThresh, 30, 255, cv::THRESH_BINARY);
+    cv::Mat imgBin;
+    cv::threshold(imgGreyBlur, imgBin, cv::mean(imgGreyBlur)[0]-10, 255, cv::THRESH_BINARY);
 
-    // Entrauschen: 2x "öffnen" mit 3x3 Kernel
+    //cv::adaptiveThreshold(imgGreyBlur, imgBin,255,CV_ADAPTIVE_THRESH_MEAN_C,CV_THRESH_BINARY,2001,10);
+    //cv::namedWindow("bin", 0);                  // Für debugging
+    //cv::imshow("bin", imgBin);                  // Für debugging
+
+    // Entrauschen: 2x öffnen mit 3x3 Kernel
     cv::Mat kernel = cv::getStructuringElement(cv::MORPH_CROSS, cv::Size(3, 3), cv::Point(1, 1));
-    cv::morphologyEx(imgThresh, imgThresh, cv::MORPH_OPEN, kernel, cv::Point(-1,-1), 2);
-
-    // Bild anzeigen
-    cv::imshow("Binary", imgThresh);
+    cv::morphologyEx(imgBin, imgBin, cv::MORPH_OPEN, kernel, cv::Point(-1,-1), 2);
 
     // Kantenerkennung:
     //cv::Mat imgEdge;
-    //cv::Canny(imgThresh, imgEdge, 50, 50);
+    //cv::Canny(imgBin, imgEdge, 50, 50);
 
     // Konturen suchen
-    cv::Mat imgThreshTemp = imgThresh;
+    cv::Mat imgThreshTemp = imgBin;
     //std::vector<std::vector<cv::Point> > contours;
     cv::findContours(imgThreshTemp, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
 
@@ -86,6 +123,7 @@ int main(int argc, char *argv[])
     }
 
     // Bild anzeigen
+    cv::namedWindow("Kontur", 0);
     cv::imshow("Kontur", imgCont);
 
     // Kontur in Datei ablegen wenn Aufrufargument == "Puzzle.exe <filename> <Konturnummer>"
